a9f7cdc2a23edb113e04bbc80906c1fbc3228344
Binhanh <binhanh080103@gmail.com>
2022-08-26 19:44:31 +0700

    fix UI/UX for large device

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Header, AboutUs, Menu, Chef, Intro, Laurels, Gallery, FindUs, Footer } from './container'
 import { Navbar } from './components' 
 import './App.scss';
+import React, { Suspense, useCallback, useEffect, useState } from 'react';
+
+
 
 function App() {
+
   return (
     <div>
       <Navbar />
